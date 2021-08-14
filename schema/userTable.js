dea4b1f55cb50e1f43e6801af3d654d371245d46
royiluz <royi.iluz@gmail.com>
2021-08-14 13:38:56 +0300

    gitignore and schema

--- schema/userTable.js
+++ schema/userTable.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
+//Define a schema
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     status: String
 });
 
+// Compile model from schema
 const userTable = mongoose.model('users', userSchema);
 
 module.exports = userTable;
